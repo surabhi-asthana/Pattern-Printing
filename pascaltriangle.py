c32a375ab6e4dd3fb2bf2e22256b50fedef73785
Surabhi <surabhiasthana14@gmail.com>
2025-09-07 20:41:14 +0530

    Add files via upload

---
+++ pascaltriangle.py
@@ -0,0 +1,20 @@
+def generate(numRows):
+    a=[[1]]
+    for i in range(0,numRows-1):
+        b=[]
+        for j in range(0,len(a[i])+1):
+            if j==0 or j==len(a[i]):
+                b.append(1)
+            else:
+                b.append(a[i][j]+a[i][j-1])
+        a.append(b)
+    return(a)
+n=int(input("Enter number of rows: "))
+print(generate(n))
+
+
+
+
+
+        
+        
